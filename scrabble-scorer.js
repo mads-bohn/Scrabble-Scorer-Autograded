9022d46a7b7c93df9afda49d85e507d0f1a18dd8
mads-bohn <mady.bohn@gmail.com>
2024-02-02 17:50:19 -0600

    modified initial prompt

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -33,7 +33,10 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   console.log("Let's play some scrabble!");
+   let userWord = input.question("Enter a word: ");
+   console.log(oldScrabbleScorer(userWord));
+   return userWord;
 };
 
 let simpleScorer;
